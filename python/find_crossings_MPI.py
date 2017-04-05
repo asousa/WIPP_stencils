7aa66bf318e5848d35b951419a991597db86db13
Austin Sousa <asousa@stanford.edu>
2017-04-04 20:38:44 -0700

    Moving to a fresh directory from 3dWIPP and lightning_power_study. This package eventually will generate the flux stencils needed for the global estimates.

---
+++ python/find_crossings_MPI.py
@@ -0,0 +1,182 @@
+import numpy as np
+from scipy import interpolate
+# import matplotlib.pyplot as plt
+import os
+import itertools
+from methods.partition import partition
+import time
+import datetime as dt
+import sys
+from methods.index_helpers import load_TS_params
+from methods.index_helpers import load_Dst
+from methods.index_helpers import load_Kp
+from methods.index_helpers import load_ae
+# from spacepy import coordinates as coord
+# from spacepy.time import Ticktock
+
+import methods.xflib  # Fortran xform-double library (coordinate transforms)
+import bisect
+
+import gzip
+import pickle
+import commands
+import subprocess
+from mpi4py import MPI
+
+from methods.find_crossings import find_crossings
+
+project_root = '/shared/users/asousa/WIPP/WIPP_stencils/'
+
+# ---------------------- Constants -------------------------
+R_E = 6371.0    # km
+R2D = 180./np.pi
+D2R = np.pi/180.
+
+# ------------------ Simulation params ---------------------
+
+# Simulation time
+ray_datenum = dt.datetime(2010, 01, 01, 0, 00, 00);
+
+# ray lats:
+ray_lat_spacing = 1
+ray_lats = np.arange(12, 60, ray_lat_spacing)
+# ray_lats = np.arange(20,25,ray_lat_spacing)
+
+tmax = 20
+dt = 0.05
+
+
+ray_lat_pairs = zip(ray_lats[0:], ray_lats[1:])
+# Frequencies
+f1 = 200; f2 = 30000;
+num_freqs = 33
+flogs = np.linspace(np.log10(f1), np.log10(f2), num_freqs)
+freqs = np.round(pow(10, flogs)/10.)*10
+freq_pairs = zip(freqs[0:], freqs[1:])
+
+
+Llims = [1.2, 8]
+Lstep = 0.1
+out_Lsh = np.arange(Llims[0], Llims[1], Lstep)
+out_lat = np.round(10.0*np.arccos(np.sqrt(1./out_Lsh))*R2D)/10.0
+
+# out_lon = [0]
+
+num_offset_lons = 0 # on each side of center
+offset_lon_spacing = 1
+
+dlat_fieldline = 1.     # degree spacing between EA segments
+model_number = 0        # b-field model (0 = dipole, 1 = IGRF)
+num_freq_steps = 20     # number of interpolating steps between 
+                        # each guide frequency.
+crossing_method = 1
+# vec_ind = 0     # Which set of default params to use for the gcpm model
+# Mean parameter vals for set Kp:
+Kpvec = [0]
+
+nightday = 'nightside'
+
+
+
+
+ray_input_directory_root = '/shared/users/asousa/WIPP/rays/2d/%s/mode6/'%nightday
+output_directory_root    = os.path.join(project_root, "outputs", "crossings",nightday)
+
+# ----------------------------------------------------------
+
+iyr = ray_datenum.year
+idoy= ray_datenum.timetuple().tm_yday 
+isec = (ray_datenum.second + (ray_datenum.minute)*60 + ray_datenum.hour*60*60)
+
+
+# -------------- set up MPI -----------------------------
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+host = commands.getoutput("hostname")
+nProcs = 1.0*comm.Get_size()
+
+# Set up output tree
+if rank==0:
+    for Kp in Kpvec:
+        output_directory = os.path.join(output_directory_root, 'kp%d'%Kp)
+        if not os.path.exists(output_directory):
+            # os.mkdir(output_directory)
+            os.system('mkdir -p %s'%output_directory)
+        
+        python_dump_path = os.path.join(output_directory,'python_data')
+        
+        if not os.path.exists(python_dump_path):
+            os.mkdir(python_dump_path)
+        
+        # for (f1, f2) in freq_pairs:
+        #     subdir = os.path.join(output_directory, 'f_%g_%g'%(f1, f2))
+        #     if not os.path.exists(subdir):
+        #         os.mkdir(subdir)
+        #     for olat in out_lat:
+        #         subsubdir = os.path.join(subdir, 'out_%g'%olat)
+        #         if not os.path.exists(subsubdir):
+        #             os.mkdir(subsubdir)
+else:
+    pass
+
+comm.Barrier()
+
+if rank==0:
+
+    tasklist = [(x,y,z) for x,y,z in itertools.product(ray_lat_pairs, freq_pairs, Kpvec)]
+    np.random.shuffle(tasklist)
+    chunks = partition(tasklist, nProcs)
+
+    print len(tasklist), "total jobs"
+else:
+    tasklist = None
+    chunks   = None
+
+comm.Barrier()
+
+tasklist = comm.bcast(tasklist, root=0)
+chunks   = comm.bcast(chunks, root=0)
+
+# Split frequency vector into smaller chunks, pass each chunk to a process
+nTasks  = 1.0*len(tasklist)
+nSteps = np.ceil(nTasks/nProcs).astype(int)
+
+
+# Run each set of jobs on current node:
+if (rank < len(chunks)):
+    print "Process %d on host %s, doing %g jobs"%(rank, host, len(chunks[rank]))
+
+    for job in chunks[rank]:
+        ray_lat_1 = job[0][0]
+        ray_lat_2 = job[0][1]
+        f1 = job[1][0]
+        f2 = job[1][1]
+        Kp = job[2]
+
+        ray_dir = os.path.join(ray_input_directory_root,'kp%d'%Kp)
+
+        if nightday == "nightside":
+            center_lon = 0.
+        else:
+            center_lon = 180.
+
+        data = find_crossings(ray_dir=ray_dir,
+                                tmax =tmax,
+                                dt = dt,
+                                lat_low = ray_lat_1,
+                                lat_step_size = ray_lat_spacing,
+                                f_low=f1, f_hi = f2,
+                                Llims = Llims,
+                                L_step = Lstep,
+                                center_lon = center_lon,
+                                dlat_fieldline = dlat_fieldline)
+
+        python_dump_path = os.path.join(output_directory,'python_data')
+
+        outfile = os.path.join(python_dump_path,'crossing_log_lat_%d-%d_f_%d-%d.pklz'%(ray_lat_1, ray_lat_2, f1, f2))
+
+        with gzip.open(outfile,'wb') as file:
+            pickle.dump(data, file)
+
+
+
